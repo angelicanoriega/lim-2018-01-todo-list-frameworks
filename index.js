fab32d614abbd2d5cd879e686ae439f518439ff4
Maria <mariaanguevara@gmail.com>
2018-10-10 09:35:53 -0500

    avance

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import main from './src/main/body';
+
+const app =  document.getElementById('app');
+
+// ReactDOM.render(que voy a renderiar, donde lo haré)
+const contect=<h1>hola mundo</h1>;
+ReactDOM.render(contect, app)
